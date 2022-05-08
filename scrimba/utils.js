557224a315e7e90dd890e20b2647bf9175ba32c4
abiton198 <abitonp@gmail.com>
2022-05-08 08:24:51 +0200

    git commit -m "Edit index.html"

--- scrimba/utils.js
+++
@@ -1,9 +0,0 @@
-
-
-this.getDiceRollArray = function(diceCount){
-    return new Array (diceCount).fill(0).map(function(){
-return Math.floor(Math.random() * 6) + 1
-    })
-}
-
-export {getDiceRollArray}
