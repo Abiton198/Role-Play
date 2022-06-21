0b8e0b2091e617be4bed4ef1e6589c6da0f4a8bd
abiton198 <abitonp@gmail.com>
2022-06-21 10:23:45 +0200

    Edit index.html

--- color-scheme-generator/main.js
+++ color-scheme-generator/main.js
@@ -1,96 +1,40 @@
-let colorsArray = []
-seedColor = document.getElementById("seed-color") //input field
-chooseColor = document.getElementById("choose-color") //button
-colors = document.getElementById("colors") //container
+let colorArray = []
 
-fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${colorMode}&count=5`, {})
+function renderColors(){
+let colorHex = document.getElementById("color-hex").value.slice(1) //input field
+let colorMode = document.getElementById("color-mode").value //selector
+//document.getElementById("copy-info").textContent = "Click the code to copy "
+document.getElementById("title").style.color = `#${colorHex}` //to change title color accordingly 
+
+fetch(`https://www.thecolorapi.com/scheme?hex=${colorHex}&mode=${colorMode}&count=5`, {})
 .then(response => response.json())
 .then(data =>  {
-    console.log(data)
-const html = data.color
-colors.innerHTML = `<div  id="display1">${html}</div>`
-document.getElementById("display1").style.background = color 
-console.log(html)
-})
-  //  color = data['color']  )
-
-
-/*
-function generateRandomColors(){
-    let colorCode = '0123456789ABCDEF'
-    let color = data['color']
-       for (let i = 0; i < 6; i++){
-        color += colorCode[Math.floor(Math.random() * 16)]
+    for (let i=0; i < data.colors.length; i++){
+        colorArray.push(data.colors[i].hex.value)
     }
-    return color
-}
-
-chooseColor.addEventListener('click', function(){
-colors = colors
-colors.innerHTML = `
-<div  id="display1">${color}</div>
-<div id="display2">${color}</div>
-<div id="display3">${color}</div>
-<div id="display4">${color}</div>
-<div id="display5">${color}</div>`
-
-})
-
-
-
-    // decide how the html is going to be returned as color after click the button===http://api.creativehandles.com/getRandomColor====805am
-
-/*
-
-function renderColor(){
-    let html = ""
-for (let color of colorsArray){ //function to display what has been returned as data
- html +=    
-display1.style.backgroundColor = color
-}
-display1.innerHTML = html 
-}
-
-
-
-
-seedColor = document.getElementById("seed-color") //input field
-chooseColor = document.getElementById("choose-color") //button
-colors = document.getElementById("colors")
-chooseColor.addEventListener('click', function(){
-function generateRandomColors(){
-    let colorCode = '0123456789ABCDEF'
-    let color = '#'
-    for (let i = 0; i < 6; i++){
-        color += colorCode[Math.floor(Math.random() * 16)]
-    }
-    //return color
-
-colors.innerHTML = `
-<div  id="display1">${color}</div>
-<div id="display2">${color}</div>
-<div id="display3">${color}</div>
-<div id="display4">${color}</div>
-<div id="display5">${color}</div>`
-}
-generateRandomColors()
-}
-)
-
-// try generate random fcolors function 0018
-
-/*
-  for(let i = 0; i < color; i++){
-            let color = data["color"]
-    
-            display1.style.backgroundColor = color
-            display1.innerHTML = color
+const myColors = colorArray.map(color =>{ // putting data to html page
+            return      `<div>
+                            <div class="colors" style="background:${color}"></div>
+                            <input class="cod-color" size="7" value=${color}>
+                        </div>`
             
-            display2.style.backgroundColor = color
-             display2.innerHTML = color
-        }
+}) 
 
 
 
-        "https://www.thecolorapi.com/id?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html"
-*/
+
+
+
+
+document.getElementById("color-schemes").innerHTML = myColors.join('') //rendering the page
+
+})
+}
+ renderColors() 
+
+ document.getElementById("colors-form").addEventListener("submit", function(e){
+        colorArray = []
+        e.preventDefault()
+        renderColors()
+    })
+   
