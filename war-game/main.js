c3a7ad668369a920b43ee348f5e0c45898c2564b
abiton198 <abitonp@gmail.com>
2022-06-23 12:32:47 +0200

    Edit index.html

--- war-game/main.js
+++ war-game/main.js
@@ -1,5 +1,9 @@
+let deck
+
 document.getElementById("new-deck").addEventListener('click', function(){
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data =>{
+        data = data.deck_id
+    console.log(data) }) 
 })
