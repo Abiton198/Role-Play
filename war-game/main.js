5735bac35cbef885e2a223c265633d05572f5291
abiton198 <abitonp@gmail.com>
2022-06-28 03:45:48 +0200

    Edit index.html

--- war-game/main.js
+++ war-game/main.js
@@ -1,22 +1,28 @@
-let deckId = ""
+let deckId = "" //object for the deck of cards from api
+let myScore = 0
+let computerScore = 0 
 const cardsContainer =  document.getElementById("cards") //div with cards at centre
 const header = document.getElementById("header") //div with 3 items at top 
 const newDeck = document.getElementById("new-deck")//New Deck:
 const newCard = document.getElementById("new-card") //Draw:
-const remainingCards = document.getElementById("remaining-cards") //Remaining Crads:
+const remainingCards = document.getElementById("remaining-cards") //Remaining Cards:
 const cardsAvailable = document.getElementById("cards-available") //cards:
-const computerScore = document.getElementById("computer-score")//computer score:
-const myScore = document.getElementById("my-score")//Me:
+const computerScoreEl = document.getElementById("computer-score")//computer score:
+const myScoreEl = document.getElementById("my-score")//Me:
 
 // bring a deck of cards
-function handleClick(){
-    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-    .then(res => res.json())
-    .then(data =>{
-        deckId = data.deck_id
-cardsAvailable.textContent = "Cards:" + data.remaining
-    console.log(data)}) 
-    }
+/*using the async() to replace the .then() ----add await*/
+async function handleClick(){
+   const res = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+   const data = await res.json() 
+
+//    .then(res => res.json())
+//     .then(data =>{
+    deckId = data.deck_id
+    cardsAvailable.textContent = "Cards:" + data.remaining //displays cards of the game
+    remainingCards.textContent = "Remaining Cards:" + " " + data.remaining //displays remaining cards at start of game
+    console.log(data)}
+    //)}
 
 // drawing newCards from the deck
     function drawNewCard(){
@@ -33,19 +39,27 @@ cardsAvailable.textContent = "Cards:" + data.remaining
     //  displaying the winner messsage on html
     const winnerText = determineCardWinner(data.cards[0], data.cards[1])
      header.innerHTML = winnerText
-
     // const remainingCardsText = determineCardWinner(data.remaining)
      remainingCards.textContent = "Remaining Cards:" + " " + data.remaining
 
+
      if(data.remaining === 0){
         newCard.disabled = true
-     }
-       
-        }))
+
+// to display results of the game to html and total score
+        if(myScore > computerScore){
+            header.textContent = "Congrats! You won the game"
+        }else if (computerScore > myScore){
+            header.textContent = "The computer won the game, Ooops!"
+        }else{
+            header.textContent = "The game is a draw!"
+        }
+                         }     
+                }
+        ))
     }
    
-  
-//button when clicked trigger event as per notification on button(eg-newDeck brings newsetof cards)
+  //button when clicked trigger event as per notification on button(eg-newDeck brings newsetof cards)
 newDeck.addEventListener('click', handleClick)
 newCard.addEventListener('click', drawNewCard)
 
@@ -58,25 +72,19 @@ const card2ValueIndex = valueOptions.indexOf(card2.value)
 
 //conditions for the outcome/results/score
 if(card1ValueIndex > card2ValueIndex){
-    return 'card1 has won'
+    computerScore++                         //recording the winner increamentaly
+    computerScoreEl.textContent = `Computer Score: ${computerScore}`  //display the result on html
+    return 'Computer has won!'          //message on display
      
 }else if (card1ValueIndex < card2ValueIndex){
-    return  'card2 has won'
+    myScore++
+    myScoreEl.textContent = `Me: ${myScore}` 
+    return  'You won!'
 
 }else{
     return 'It is a draw'
 }}
 
-function playerScore(){
-
-    if(card1 > card2){
-    computerScore.textContent = "Computer Score:" + Number 
-}else if (card2 > card1){
-    myScore.textContent = "Me:" + Number
-}else{
- 
-}
-}
 
 //determineCardWinner(card1Obj, card2Obj)
 
