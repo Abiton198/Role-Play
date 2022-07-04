4f235d2426d4a8a2fc6fd7edd0e5085ac7d37fa8
abiton198 <abitonp@gmail.com>
2022-07-04 10:33:33 +0200

    Edit index.html

--- dashboard-api/main.js
+++ dashboard-api/main.js
@@ -1,13 +1,28 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
 .then(res => res.json())
 .then(data =>{
-    console.log(data)
     document.body.style.backgroundImage = `url(${data.urls.full})`
     document.getElementById("author").innerHTML = `By: ${data.user.name}`
+    throw Error
 })
+// use the default background image when error occurs
+.catch( err => {
+ document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&
+    cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
+   console.log(err)
+    document.getElementById("author").textContent = `By: Dodi Achmad`
+}) 
 
+fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
+.then(res =>res.json())
+.then(data => {
 
-
+    document.getElementById("crypto-top").innerHTML = `
+     <img src=${data.image.small}/> <br> ${data.name} `
+     document.getElementById("currency").innerHTML = `<p>  Current-Price: ${data.market_data.current_price.usd} </p>
+     <p> High-Price: ${data.market_data.high_24h.usd} </p> <p> Low-Price: ${data.market_data.low_24h.usd} </p>`
+    console.log(data)})
+    .catch(err => console.log(err))
 
 
 
