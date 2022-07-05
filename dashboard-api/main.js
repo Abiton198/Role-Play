0bf519f740798f7998db0cb72152aeb53ec6dcbc
abiton198 <abitonp@gmail.com>
2022-07-05 17:46:35 +0200

    Edit index.html

--- dashboard-api/main.js
+++ dashboard-api/main.js
@@ -17,16 +17,16 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 
 /*Displaying Crypto currency of choice on live updates */
 fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
-.then(res =>res => res.json())
+.then(res => res.json())
     
 .then(data => {
-
+    console.log(data)
     document.getElementById("crypto-top").innerHTML = ` 
      <img src= ${data.image.small} /> <br> ${data.name} ` //displaying logo & name
 
      document.getElementById("currency").innerHTML = `<p>  Current-Price: $ ${data.market_data.current_price.usd} </p>
      <p> High-Price: $ ${data.market_data.high_24h.usd} </p> <p> Low-Price: $ ${data.market_data.low_24h.usd} </p>`
-    console.log(data)}) 
+   }) 
     .catch(err => console.log(err))
 
 /*showing current live-time on page */
@@ -52,8 +52,8 @@ navigator.geolocation.getCurrentPosition( position => {
         const urlImage = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
         document.getElementById("weather").innerHTML =
         `<img src= ${urlImage} />
-        <p>${Math.round(data.main.temp)} ْ</p>
-        <p>${data.name}</p>
+        <p class="weather-temp">${Math.round(data.main.temp)} ْ</p>
+        <p class="weather-city">${data.name}</p>
        `
         
        })
