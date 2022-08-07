7697d1b87fc29dea38bfea5f2ed1e8b1501dfa72
abiton198 <abitonp@gmail.com>
2022-08-08 01:57:09 +0200

    .

--- digital-ocean-tutorial/src/App.js
+++ digital-ocean-tutorial/src/App.js
@@ -1,9 +1,14 @@
+import Footer from "./Footer";
 import Header from "./Header";
+import MainContent from "./Main";
 
 function App() {
  return (
-          <Header/>
-          
+          <>
+            <Header/>
+            <MainContent/>
+            <Footer/>
+          </>
           )
 }
 
--- digital-ocean-tutorial/src/Header.js
+++ digital-ocean-tutorial/src/Header.js
@@ -7,40 +7,24 @@ export default function Header(){
     return (
         <div className="App">
           <header className="App-header">
-            <img src={abiton} className="App-logo" alt="logo" />
-            <p>
-              Putting this learning to good use
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
+            <nav className="personal-info">
+                <img src={abiton} className="App-logo" alt="" />
+                <h1 className="name">Abiton Padera</h1>
+                <p className="title">Front End Developer</p>
+                <p className="email"> abitondev@gmail.com</p>
+             </nav>
+                
+                {/* social media icons */}
+             <div className="contacts">
+                <button className="email-btn">
+                <i className="fa-solid fa-envelope"></i>
+                <span> Email </span> </button>
+                <button className="linked-btn">
+                <i className="fa-brands fa-linkedin"></i>
+                <span>Linked</span> </button>
+            </div>
+          
           </header>
         </div>
       );
 }
-
-// return (
-//     <div className="header">
-//         <nav className="personal-info">
-//             <img src="./img/abiton.jpeg"/>
-//             <h1 className="name">Abiton Padera</h1>
-//             <p className="title">Front End Developer</p>
-//             <p className="email"> abitondev@gmail.com</p>
-//         </nav>
-        
-//         <div className="contacts">
-//             <button className="email-btn">
-//             <i className="fa-solid fa-envelope"></i>
-//            <span> Email </span> </button>
-//             <button className="linked-btn">
-//             <i className="fa-brands fa-linkedin"></i>
-//             <span>Linked</span> </button>
-//         </div>
-           
-//     </div>
-// )
