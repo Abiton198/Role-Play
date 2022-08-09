891e17eded1b361ad53f070d8e133d0860958823
abiton198 <abitonp@gmail.com>
2022-08-09 05:26:30 +0200

    .

--- AirBnb-experience/src/App.jsx
+++ AirBnb-experience/src/App.jsx
@@ -3,22 +3,27 @@ import Nav from './Nav'
 import Card from './Card'
 import './App.css'
 import Hero from './Hero'
+import Data from './Data'
 
+// using .map to map over data from various arrays and display it without changing our code
 function App() {
-  
+  const cards = Data.map(item => {
+    return (
+        <Card
+            key={item.id}
+            item={item}  
+            // {...item} - can be used and delete all .item in the Card.jsx
+        />
+    )
+  })
   return (
     <>
         <Nav/>
         <Hero/>
-        <Card
-            img="Katie-img.jpg"
-            rating="5.0"
-            reviewCount={6}
-            location="USA"
-            title="Life Lessons with Katie Zaferes"
-            price={136}
-            
-        />
+        <section className='cards-list'>
+            {cards}
+        </section>
+              
     </>
   )
 }
